ed894bc6b2f3e6ad22e54b956e1523bfd3abbe54
vipin <vipinkesari@gmail.com>
2020-04-16 19:38:57 +0530

    # warning removed of android

--- app/src/main/java/com/myinfosysprogram/constants/AppConstants.kt
+++ app/src/main/java/com/myinfosysprogram/constants/AppConstants.kt
@@ -1,7 +1,6 @@
 package com.myinfosysprogram.constants
 
 class AppConstants {
-
     companion object {
         const val KEY_HIDE_SPLASH: String = "key_hide_splash"
         const val PREF_NAME: String = "my_pref_com_infosys_program"
