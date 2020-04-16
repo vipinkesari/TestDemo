2e24e4ab50bae259b39e8dd01524bcf4cedbb696
vipin <vipinkesari@gmail.com>
2020-04-16 18:26:29 +0530

    # warning removed of kotlin and android

--- app/build/tmp/kapt3/stubs/debug/com/myinfosysprogram/retrofit/AppExecutors.java
+++ app/build/tmp/kapt3/stubs/debug/com/myinfosysprogram/retrofit/AppExecutors.java
@@ -2,22 +2,12 @@ package com.myinfosysprogram.retrofit;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0006\u0018\u0000 \t2\u00020\u0001:\u0001\tB\u0007\b\u0016\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u0007\u001a\u00020\u0004J\u0006\u0010\b\u001a\u00020\u0004R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\n"}, d2 = {"Lcom/myinfosysprogram/retrofit/AppExecutors;", "", "()V", "mDiskIO", "Ljava/util/concurrent/Executor;", "mMainThread", "mNetworkIO", "diskIO", "mainThreadIO", "Companion", "app_debug"})
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\u0018\u0000 \u00072\u00020\u0001:\u0001\u0007B\u0005\u00a2\u0006\u0002\u0010\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\b"}, d2 = {"Lcom/myinfosysprogram/retrofit/AppExecutors;", "", "()V", "mDiskIO", "Ljava/util/concurrent/Executor;", "mMainThread", "mNetworkIO", "Companion", "app_debug"})
 public final class AppExecutors {
     private java.util.concurrent.Executor mDiskIO;
     private java.util.concurrent.Executor mNetworkIO;
     private java.util.concurrent.Executor mMainThread;
     public static final com.myinfosysprogram.retrofit.AppExecutors.Companion Companion = null;
-    
-    @org.jetbrains.annotations.NotNull()
-    public final java.util.concurrent.Executor diskIO() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.NotNull()
-    public final java.util.concurrent.Executor mainThreadIO() {
-        return null;
-    }
     
     public AppExecutors() {
         super();
